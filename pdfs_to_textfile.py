adf9a8856857a6e4a83871e339bdce5f6efe9473
Geo <mrgeooo14@gmail.com>
2023-04-04 01:55:17 +0200

    finished word2vec - NER Spacy Start

--- pdfs_to_textfile.py
+++ pdfs_to_textfile.py
@@ -24,8 +24,8 @@ def write_pdfs_to_text_file(pdf_directory: str, text_file_path : str):
 #### The two parameters that the function needs are the path of the text file (empty) ###########
 #### And the directory that includes the pdf documents that will be read and written ############  
 if __name__ == "__main__":
-    text_file_path = 'data/train_sets/train_100_books.txt'
-    pdfs_directory = 'data/implementome_publications/train_set_100_books/'
+    text_file_path = 'data/train_sets/train_100_books2.txt'
+    pdfs_directory = 'data/implementome_publications/train_set_100_books2/'
     print('Writing PDFs to the provided text file started...')
     write_pdfs_to_text_file(pdfs_directory, text_file_path)
     print('Process finished')
