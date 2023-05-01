8ddf21399673faf135cc65607d604b57a9b6a839
Geo <mrgeooo14@gmail.com>
2023-05-02 01:25:45 +0200

    lenient token level ner scoring system

--- ner_score_report.py
+++ ner_score_report.py
@@ -1,4 +1,5 @@
 from seqeval.metrics import classification_report as seqclassify
+from seqeval.scheme import IOB2
 
 #### NOTE: The functions expect a tag dictionary of 'true' and 'predicted' keys 
 #### The keys hold entity true values and predictions according to the IOB2 Schema
@@ -16,19 +17,14 @@ def set_tags_to_fixed_labels(entity_labels, tags_lists):
 
 
 #### Compute and print the evaluation scores using seqeval (F1 Score, Precision and Recall per label, Micro and Macro Averages)
-def compute_and_print_scores(entity_labels, prediction_tags):
-    print('Performance with only our needed tags: ')
-    
-    ##### Set any IOB tag that contains a different entity label than our wanted ones to 'O' (Others)
-    prediction_tags['true'] = set_tags_to_fixed_labels(entity_labels, prediction_tags['true'])
-    prediction_tags['predicted'] = set_tags_to_fixed_labels(entity_labels, prediction_tags['predicted'])
+def compute_and_print_strict_scores(iob_tags):
+    print('Performance with respect to only our fixed labels: ')
     
     ##### seqclassify classification report taking as inputs the true tags in addition to the predicted ones
-    score_report = seqclassify(prediction_tags['true'], prediction_tags['predicted'], zero_division = 1)
-    strict_score_report = seqclassify(prediction_tags['true'], prediction_tags['predicted'], mode = 'strict', zero_division = 1)
+    score_report = seqclassify(iob_tags['true'], iob_tags['predicted'], zero_division = 1, scheme = IOB2)
+    strict_score_report = seqclassify(iob_tags['true'], iob_tags['predicted'], mode = 'strict', zero_division = 1, scheme = IOB2)
 
-    print('\nScore Report:')
-    print('\n'.join(score_report.splitlines()))
-
-    print('\nStrict Mode:')
+    # print('\n'.join(score_report.splitlines()))
+    print('\nStrict Score Report (Entity Span-Level):')
     print('\n'.join(strict_score_report.splitlines()))
+    
